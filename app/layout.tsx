5594473c2b79a02f3cf6c367a23901668895c256
tomlynch96 <tomlynch96@gmail.com>
2026-03-29 16:08:22 +0100

    attempt to fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,300&family=DM+Mono:wght@400;500&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
